5d76fc50e42e7df7f44a5e4a71cf1e1cf25d4693
kitety <aizaizuori@foxmail.com>
2024-10-15 21:58:18 +0800

    feat: event detail page

--- app/event/[id].tsx
+++ app/event/[id].tsx
@@ -1,14 +1,29 @@
-import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import dayjs from 'dayjs';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { Image, Text, View } from 'react-native';
 import events from '~/assets/events.json';
 
 const EventPage = () => {
   const { id } = useLocalSearchParams();
   const event = events.find((event) => event.id === id);
+  if (!event) {
+    return <Text>event not found</Text>;
+  }
+  const dayjsObject = dayjs(event.datetime);
+  const timeStr = dayjsObject.format('ddd D MMM · hh:mm A');
   return (
-    <View>
-      <Text>event detail page{event?.title}</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{ title: 'Event', headerBackTitleVisible: false, headerTintColor: 'black' }}
+      />
+      <View className="flex-1 gap-3  bg-white p-3">
+        {/* image */}
+        <Image className="aspect-video w-full rounded-3xl" source={{ uri: event.image }} />
+        <Text className="text-3xl font-bold">{event.title}</Text>
+        <Text className="text-lg font-semibold uppercase text-amber-800">{timeStr}</Text>
+        <Text className="text-lg">{event.description}</Text>
+      </View>
+    </>
   );
 };
 
