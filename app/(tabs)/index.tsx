8e70c6e03de1a776e89738ddf51134b160f2f447
kitety <aizaizuori@foxmail.com>
2024-10-15 13:55:01 +0800

    feat: event item

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,33 @@
+import Feather from '@expo/vector-icons/Feather';
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { Image, Text, View } from 'react-native';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+      <Stack.Screen options={{ title: 'Events' }} />
+      <View className="gap-3 p-3">
+        <View className="flex-row">
+          <View className="flex-1 gap-2">
+            <Text className="text-lg font-semibold uppercase text-amber-800">
+              Wed 13, Sep · 19:30 CET
+            </Text>
+            <Text className="text-xl font-bold">This is a title</Text>
+            <Text className="text-gray-700">City hall</Text>
+          </View>
+          {/* image */}
+          <Image
+            className="aspect-video w-2/5 rounded-lg"
+            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg' }}
+          />
+        </View>
+        {/* footer */}
+        <View className="flex-row gap-3">
+          <Text className="mr-auto text-gray-700">16 going</Text>
+          <Feather color="gray" name="share" size={20} />
+          <Feather color="gray" name="bookmark" size={24} />
+        </View>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
